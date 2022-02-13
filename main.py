bb9ea201e17241e8ffa06a52cea0e185828e4e5a
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-02-13 17:27:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+Given a string as input consisting of digits. Create an int which has the same value as the number represented by the String.
+Input-> "1234"
+Output-> 1234
+"""
+
+
+st = "1234"
+print(int(st))
